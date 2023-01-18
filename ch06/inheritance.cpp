6fe60ae7cc243b87cf0ba2e01fbf31404e1382ce
Taehoon Kim <taehoonkim.dev@gmail.com>
2023-01-18 11:00:29 +0900

    Add chapter 06

---
+++ ch06/inheritance.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class Base
+{
+    std::string s;
+
+public:
+    Base() : s("기반") { std::cout << "기반 클래스" << std::endl; }
+
+    void what() { std::cout << s << std::endl; }
+};
+
+class Derived : public Base
+{
+    std::string s;
+
+public:
+    Derived() : Base(), s("파생")
+    {
+        std::cout << "파생 클래스" << std::endl;
+
+        // Base 에서 what() 을 물려 받았으므로
+        // Derived 에서 당연히 호출 가능하다
+        what();
+    }
+};
